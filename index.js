2416508fb683737a14db97f4703626b686333169
Harryexe74 <junaidmahmood141@gmail.com>
2024-10-11 10:21:37 +0500

    index.js

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-// import config from "./config/index.js";
-// import connectDB from "./config/db.js";
+import config from "./config/index.js";
+import connectDB from "./config/db.js";
 
-// import app from "./app.js";
-
-// connectDB();
-
-// const port = config.port || 4000;
-
-// app.listen(port, () => {
-// 	console.log(`Server is listening on port ${port}`);
-// });
-
-import express from 'express';
-import connectDB from './config/db.js';
-
-const app = express();
-
-// Middleware, routes, etc.
+import app from "./app.js";
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = config.port || 4000;
+
+app.listen(port, () => {
+	console.log(`Server is listening on port ${port}`);
 });
 
-export default app;
+
 
 
